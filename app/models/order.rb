235a0e8fd9d8c6c3c88a2298e3b0d3dfa7e1382e
kos-zenin <kos.zenin@gmail.com>
2015-12-13 07:33:40 +0300

    add simple state machine

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,24 @@ class Order < ActiveRecord::Base
   has_and_belongs_to_many :addresses
   has_and_belongs_to_many :transports
   has_many :deals
+  has_one :assigned_deal, -> { where(status: Deal::IN_PROGRESS) }, class_name: :Deal
+  has_one :courier, through: :assigned_deal
+
+  state_machine :status, initial: :opened do
+    state :opened
+    state :assigned
+    state :closed
+
+    event :assign do
+      transition :opened => :assigned
+    end
+
+    event :reopen do
+      transition :assigned => :opened
+    end
+
+    event :close do
+      transition [:opened, :assigned] => :closed
+    end
+  end
 end
