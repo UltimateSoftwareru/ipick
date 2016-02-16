4fa20d6ca566ad10068b1e6e536aab255f13d8b6
kos-zenin <kos.zenin@gmail.com>
2016-02-16 14:34:23 +0300

    add leaflet map icons

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :use_sudo, true
 
 set :log_level, :info
 set :linked_files, %w{config/database.yml config/secrets.yml config/client.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/assets}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.3'
